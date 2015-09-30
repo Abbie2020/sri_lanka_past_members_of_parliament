0ca75a9e9747b7c87b8b1d953522381037d93e10
Abigail Rumsey <abbie_is@hotmail.com>
2015-09-30 17:13:41 +0100

    Added my own scraper.py code

--- scraper.py
+++ scraper.py
@@ -1,24 +1,76 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+from lxml import html
+import requests
+import re
+import scraperwiki
+import time
+import string
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+def do_the_scraping():
+    index_url = "http://www.parliament.lk/members-of-parliament/index2.php?option=com_members&task=past&letter="    
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+    for letter in string.ascii_uppercase:
+        data = requests.get(index_url + letter).json()
+        for item in data:
+            url = "http://www.parliament.lk/en/members-of-parliament/directory-of-members/viewMember/" + str(item['mem_intranet_id'])
+            scrape_mp(url)
+            time.sleep(0.5)
+
+def scrape_mp(url):
+    page = requests.get(url)
+    tree = html.fromstring(page.text)
+    id = url.rsplit('/',1)[1]
+    find_mp_name = tree.xpath('//div[@class="components-wrapper"]/h2/text()') 
+    if find_mp_name:
+        name = find_mp_name[0].split(',',1)[0]
+    else:
+        name = ""
+    find_party = tree.xpath('//td[div="Last elected Party"]/a[1]/text()')
+    if find_party:
+        party_list = find_party[0].split('(',1)
+        party = party_list[0]
+        party_id = party_list[1].split(')',1)[0]
+    else:
+        party = ""  
+        party_id = ""                              
+    find_district = tree.xpath('//td[div="Electoral District / National List"]/text()') 
+    if find_district:
+        area = find_district[0].strip()
+    else:
+        area = ""
+    find_email = tree.xpath('//a[@onclick="getContactUs();"]/text()') 
+    if find_email:
+        email = find_email[0]
+    else:
+        email = ""
+    find_image = tree.xpath('//div[@class="left-pic"]/img/@src') 
+    if find_image:
+        image = find_image[0]
+    else:
+        image = ""
+    find_birth_date = tree.xpath('//td[span="Date of Birth"]/text()') 
+    if find_birth_date:
+        birth_date = find_birth_date[0].strip()
+        birth_date = re.search(r'\d{1,2}-\d{1,2}-\d{2,4}', birth_date).group()
+        birth_date = time.strptime(birth_date,'%d-%m-%Y')
+        try:
+            birth_date = time.strftime('%Y-%m-%d',birth_date)
+        except:
+            birth_date = ""
+    else:
+        birth_date = ""
+
+    data = {
+        'id': id,
+        'name': name,
+        'area': area,
+        'party': party,
+        'party_id': party_id,
+        'email': email,
+        'image': image,
+        'birth_date': birth_date,
+        'source': url,
+        }
+    print data
+    scraperwiki.sqlite.save(unique_keys = ['id'], data = data)
+        
+do_the_scraping()
